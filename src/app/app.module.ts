82509432b22857f719d7cf57fd4f56d767c069c1
Matek1731 <matek.sl@wp.pl>
2019-01-05 14:11:05 +0100

    map and search bar components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    SearchBarComponent,
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
